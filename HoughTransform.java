bc0c59e42ef0a2687324b6be6a3dafa8181e6d55
justin <jpdd65@mun.ca>
2019-03-08 12:53:20 -0330

    Added edge detection (at least for straight lines) using Sobel filter

--- HoughTransform.java
+++ HoughTransform.java
@@ -2,6 +2,7 @@ import java.awt.*;
 import java.awt.event.*;
 import java.awt.image.*;
 import java.io.*;
+import java.nio.Buffer;
 import javax.imageio.*;
 
 // Main class
@@ -59,12 +60,63 @@ public class HoughTransform extends Frame implements ActionListener {
 			System.exit(0);
 		}
 	}
+
+    public static double convolution(int[][] pixelMatrix) {
+
+        int gy = (pixelMatrix[0][0] * -1) + (pixelMatrix[0][1] * -2) + (pixelMatrix[0][2] * -1) + (pixelMatrix[2][0]) + (pixelMatrix[2][1] * 2) + (pixelMatrix[2][2] * 1);
+        int gx = (pixelMatrix[0][0]) + (pixelMatrix[0][2] * -1) + (pixelMatrix[1][0] * 2) + (pixelMatrix[1][2] * -2) + (pixelMatrix[2][0]) + (pixelMatrix[2][2] * -1);
+        return Math.sqrt(Math.pow(gy, 2) + Math.pow(gx, 2));
+
+    }
+
+    // display the spectrum of the transform.
+    public void DisplayTransform(int wid, int hgt, int[][] g) {
+        target.resetBuffer(wid, hgt);
+        for (int y = 0, i = 0; y < hgt; y++)
+            for (int x = 0; x < wid; x++, i++) {
+                int value = g[y][x] > 255 ? 255 : g[y][x];
+                target.image.setRGB(x, y, new Color(value, value, value).getRGB());
+            }
+        target.repaint();
+    }
+
+
+    public static void main(String[] args) {
+        new HoughTransform(args.length == 1 ? args[0] : "rectangle.png");
+    }
+
 	// Action listener
 	public void actionPerformed(ActionEvent e) {
 		// perform one of the Hough transforms if the button is clicked.
 		if ( ((Button)e.getSource()).getLabel().equals("Line Transform") ) {
 			int[][] g = new int[360][diagonal];
-			// insert your implementation for straight-line here.
+            int maxRadius = (int) Math.ceil(Math.hypot(width, height));
+            BufferedImage outputImg = new BufferedImage(input.getWidth(), input.getHeight(), BufferedImage.TYPE_INT_RGB);
+            int[][] pixelMatrix = new int[3][3];
+            try {
+                for (int i = 1; i < input.getWidth() - 1; i++) {
+                    for (int j = 1; j < input.getHeight() - 1; j++) {
+                        pixelMatrix[0][0] = new Color(input.getRGB(i - 1, j - 1)).getRed();
+                        pixelMatrix[0][1] = new Color(input.getRGB(i - 1, j)).getRed();
+                        pixelMatrix[0][2] = new Color(input.getRGB(i - 1, j + 1)).getRed();
+                        pixelMatrix[1][0] = new Color(input.getRGB(i, j - 1)).getRed();
+                        pixelMatrix[1][2] = new Color(input.getRGB(i, j + 1)).getRed();
+                        pixelMatrix[2][0] = new Color(input.getRGB(i + 1, j - 1)).getRed();
+                        pixelMatrix[2][1] = new Color(input.getRGB(i + 1, j)).getRed();
+                        pixelMatrix[2][2] = new Color(input.getRGB(i + 1, j + 1)).getRed();
+
+                        int edge = (int) convolution(pixelMatrix);
+                        outputImg.setRGB(i, j, (edge << 16 | edge << 8 | edge));
+                    }
+                }
+
+                // New image with sobel filter applied for edge detection will be written to "test1" image file
+                File outputfile = new File("test1.jpg");
+                ImageIO.write(outputImg, "jpg", outputfile);
+            } catch (IOException ex) {
+                System.err.println("Image width:height=" + input.getWidth() + ":" + input.getHeight());
+            }
+
 			DisplayTransform(diagonal, 360, g);
 		}
 		else if ( ((Button)e.getSource()).getLabel().equals("Circle Transform") ) {
@@ -73,20 +125,5 @@ public class HoughTransform extends Frame implements ActionListener {
 			// insert your implementation for circle here.
 			DisplayTransform(width, height, g);
 		}
-	}
-	// display the spectrum of the transform.
-	public void DisplayTransform(int wid, int hgt, int[][] g) {
-		target.resetBuffer(wid, hgt);
-		for ( int y=0, i=0 ; y<hgt ; y++ )
-			for ( int x=0 ; x<wid ; x++, i++ )
-			{
-				int value = g[y][x] > 255 ? 255 : g[y][x];
-				target.image.setRGB(x, y, new Color(value, value, value).getRGB());
-			}
-		target.repaint();
-	}
-
-	public static void main(String[] args) {
-		new HoughTransform(args.length==1 ? args[0] : "rectangle.png");
 	}
 }
